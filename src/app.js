0398e01640ed03fa4f7e358ef8de47b202e53ffe
navia <naviamatute@yahoo.com>
2023-11-05 17:59:54 -0500

    sentence

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let pro = ["The", "our"];
+let adj = ["big", "great"];
+let noun = ["jogger", "racoon"];
+let extension = ["com", "net"];
+
+function randomElement(arr) {
+  return arr[Math.round(Math.random() * arr.length)];
+}
+
+let sentence = `${randomElement(pro)} ${randomElement(adj)} ${randomElement(noun)} ${randomElement(extension)}`;
+  console.log("start", sentence);
+
+  const paraElement = document.getElementsByTagName("p");
+
+  paraElement[0].innerText = sentence;
 };
+
+
+
+
+
+
